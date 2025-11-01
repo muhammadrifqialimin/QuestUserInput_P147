f118abb2893de741e9f6fd1d8d2b3d8e91b6664b
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-01 14:16:22 +0700

    Menambahkan variabel state untuk input pada FormData

--- app/src/main/java/com/example/questuserinput_p147/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_p147/TeksEdit.kt
@@ -1,2 +1,14 @@
 package com.example.questuserinput_p147
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormData(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textNikah by remember { mutableStateOf("") }
