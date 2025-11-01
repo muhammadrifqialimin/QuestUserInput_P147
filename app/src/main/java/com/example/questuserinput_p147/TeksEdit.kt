3ea8a4c82bcc1ae8c0e9f939e8bd104cdb2b60d8
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-01 14:22:50 +0700

    Menambahkan teks judul "Formulir Pendaftaran" pada header dengan alignment kiri dan padding

--- app/src/main/java/com/example/questuserinput_p147/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_p147/TeksEdit.kt
@@ -6,16 +6,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormData(modifier: Modifier = Modifier) {
@@ -42,5 +47,14 @@ fun FormData(modifier: Modifier = Modifier) {
                 .background(
                     brush = Brush.verticalGradient(listOf(purple, purpleDark)),
                     shape = RoundedCornerShape(bottomStart = 0.dp, bottomEnd = 0.dp)
-                ),
+                ),contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = "Formulir Pendaftaran",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(horizontal = 20.dp)
+            )
+        }
 
