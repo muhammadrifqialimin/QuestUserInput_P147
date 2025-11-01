be6e70573525f680aab3b99f0f0b1089ec569961
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-01 14:17:12 +0700

    Refactor nama variabel state pada form menjadi lebih singkat

--- app/src/main/java/com/example/questuserinput_p147/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_p147/TeksEdit.kt
@@ -1,6 +1,7 @@
 package com.example.questuserinput_p147
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -12,3 +13,8 @@ fun FormData(modifier: Modifier = Modifier) {
     var textAlamat by remember { mutableStateOf("") }
     var textJK by remember { mutableStateOf("") }
     var textNikah by remember { mutableStateOf("") }
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+    var nikah by remember { mutableStateOf("") }
